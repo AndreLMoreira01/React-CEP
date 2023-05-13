801fb59e401f91df2efd9a6d6578fd433f19143c
André L Moreira <meuimeireco@gmail.com>
2023-05-13 12:12:45 -0300

    Buscador de CEP
    Não Finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+import { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <h1 className="title">
+        BUSCADOR CEP
+        </h1>
+
+      <div class="containerInput">
+      
+      <input
+      type="text"
+      placeholder="Digite seu CEP"      
+      
+      
+      />
+
+      <button className="buttonSearch">
+      <FiSearch size={25} color='#FFF'>
+
+      </FiSearch>
+      </button>
+      </div>
+
+      <main className='main'>
+        <h2>
+          CEP: 
+        </h2>
+
+        <span>Rua:</span>
+        <span>Complemento:</span>
+        <span>Bairro:</span>
+        <span>Estado:</span>
+
+      </main>
+    
     </div>
   );
 }
